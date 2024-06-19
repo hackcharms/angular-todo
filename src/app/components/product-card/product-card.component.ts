5245dcf31e206fe6af21e0c92a6be01056b3c631
hackcharms <zaa78692@gmail.com>
2024-06-19 15:33:41 +0530

    added showcase for directives

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,14 +1,31 @@
 import { Component, Input } from '@angular/core';
 import { PRODUCT_DATA } from '../../../constants';
+import {
+  NgClass,
+  NgIf,
+  NgStyle,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+} from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgStyle, NgClass, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
   @Input({})
   product = PRODUCT_DATA;
+  @Input({})
+  label: 'fast-selling' | 'limited-deal' = 'fast-selling';
+
+  ratingColor() {
+    return {
+      good: this.product.rating.rate <= 3,
+      excellent: this.product.rating.rate > 3,
+    };
+  }
 }
