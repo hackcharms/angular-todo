8682b76bf1e0937ecaccb397573111715ec95e30
hackcharms <zaa78692@gmail.com>
2024-06-19 15:46:47 +0530

    ngFor directve and controll flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ClickCounterComponent } from './components/click-counter/click-counter.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductCardComponent, ClickCounterComponent],
+  imports: [RouterOutlet, ProductCardComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
